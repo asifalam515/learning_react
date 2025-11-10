72d90f8e6bef34b622883e12e5cc77474ff0375d
asifalam515 <asibulalam515@gmail.com>
2025-11-10 14:15:59 +0600

    state updated

--- adding_interactivity/src/Button.jsx
+++ adding_interactivity/src/Button.jsx
@@ -1,13 +1,29 @@
-export default function Signup() {
+import { useState } from "react";
+
+export default function Form() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState("Hi!");
+  if (isSent) {
+    return <h1>Your message is on its way!</h1>;
+  }
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        alert("Submitting!");
+        setIsSent(true);
+        sendMessage(message);
       }}
     >
-      <input />
-      <button>Send</button>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
     </form>
   );
+}
+
+function sendMessage(message) {
+  // ...
 }
