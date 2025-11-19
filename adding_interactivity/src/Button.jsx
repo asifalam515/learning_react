ea4db40360f750bca2a53fa2222a4af1c53cfd75
asifalam515 <asibulalam515@gmail.com>
2025-11-19 14:11:59 +0600

    updating array in state

--- adding_interactivity/src/Button.jsx
+++ adding_interactivity/src/Button.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+let initialCounters = [0, 0, 0];
+
+export default function CounterList() {
+  const [counters, setCounters] = useState(initialCounters);
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setCounters(nextCounters);
+  }
 
   return (
-    <>
-      <h1>{number}</h1>
-      <button
-        onClick={() => {
-          setNumber((n) => n + 1);
-          setNumber(number + 5);
-        }}
-      >
-        Increase the number
-      </button>
-    </>
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button
+            onClick={() => {
+              handleIncrementClick(i);
+            }}
+          >
+            +1
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
