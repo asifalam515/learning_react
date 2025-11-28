7ce235515b812bae36b71877c7e51fea50b0d527
asifalam515 <asibulalam515@gmail.com>
2025-11-28 06:41:26 +0600

    mirror created

--- managing_state/src/App.jsx
+++ managing_state/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-import Mirror from "./components/Mirror";
+import Menu from "./components/Mirror";
 
 const App = () => {
-  const [color,setColor] = useState("red")
-  const handleChangeColor = ()=>{
-    setColor("blue")
-  }
+ 
   return (
     <div>
-      <Mirror messageColor={color}></Mirror>
-      <br />
-      <button className="btn btn-primary" onClick={handleChangeColor}>Change Color from Parent</button>
-    
+      
+    <Menu></Menu>
     </div>
   );
 };
