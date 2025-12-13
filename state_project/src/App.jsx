b370cf17d4099f4a420fe3daa6587cedd873d89a
asifalam515 <asibulalam515@gmail.com>
2025-12-13 21:49:53 +0600

    added

--- state_project/src/App.jsx
+++ state_project/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
     <div>
           <h1>Prague itinerary</h1>
           <p>Here i am going to deep dive about state in react</p>
+          <p>state is something you can't even think of it</p>
          <AddTask></AddTask>
         <TaskList tasks={tasks}></TaskList>
     </div>
