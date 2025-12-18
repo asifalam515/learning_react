6584799fdde37cb6b08707638da3b498bad59fad
asifalam515 <asibulalam515@gmail.com>
2025-12-18 11:35:47 +0600

    context started

--- context/src/components/Page.jsx
+++ context/src/components/Page.jsx
@@ -1,16 +1,24 @@
 import Heading from "./Heading";
-import Section from "./Section";
-
- function Page() {
+import Section from './Section';
+export default function Page() {
   return (
-    <Section>
-      <Heading level={1}>Title</Heading>
-      <Heading level={2}>Heading</Heading>
-      <Heading level={3}>Sub-heading</Heading>
-      <Heading level={4}>Sub-sub-heading</Heading>
-      <Heading level={5}>Sub-sub-sub-heading</Heading>
-      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
     </Section>
   );
 }
-export default Page
